605f7a9e34cdd5422155a20bdf9332c6494f4f2b
carlos diaz <diazmelian.carlos@gmail.com>
2022-01-22 15:21:28 +0000

    created views

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,18 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+p "Cleaning database"
+Restaurant.destroy_all
+p "DB cleaned"
+
+p "creating restaurants"
+100.times do
+  restaurant = Restaurant.create(
+    name: "Res Name",
+    address:"1234 ocxc vale",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
+
+p "done"
